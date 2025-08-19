465cb287f4249bbfe2c2e77836731e551ff0ce68
Brian Webb <brianwebb22@gmail.com>
2025-08-19 10:54:08 -0700

    0.0.1 binary

---
+++ FlyreelMeshScan.xcframework/ios-arm64/FlyreelMeshScan.framework/Headers/FlyreelMeshScan.h
@@ -0,0 +1,18 @@
+//
+//  FlyreelMeshScan.h
+//  FlyreelMeshScan
+//
+//  Created by White, Jeffrey (RIS_HBE) on 5/9/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlyreelMeshScan.
+FOUNDATION_EXPORT double FlyreelMeshScanVersionNumber;
+
+//! Project version string for FlyreelMeshScan.
+FOUNDATION_EXPORT const unsigned char FlyreelMeshScanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlyreelMeshScan/PublicHeader.h>
+
+
